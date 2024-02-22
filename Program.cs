a034b9ed8261379cb36242c373ac98c86d73da73
maristuela <110262787+maristuela@users.noreply.github.com>
2024-02-22 22:45:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstApp
+
+
+
+
+{ /*Моя первая программа.*/
+    class Progr
+    {
+        static void Main(string[] args) // Метод Main - точка входа в приложение
+        {
+            Console.WriteLine("Hello World!"); // WriteLine - выводит строку
+
+
+
+
+
+        }
+    }
+}
